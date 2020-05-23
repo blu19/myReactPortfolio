dad8e4efedf77a60567b218eb38d584a8b45b0b5
BrianU <brian.ulatowski@gmail.com>
2020-05-22 19:53:25 -0700

    navbar items

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -10,7 +10,7 @@ class Header extends Component {
             isOpenSidebar: false,
         };
 
-        this.openNavbar = this.openNavbar.bind(this);
+        this.openSidebar = this.openSidebar.bind(this);
         this.onHandleClickLink = this.onHandleClickLink.bind(this);
     };
 
@@ -52,136 +52,141 @@ class Header extends Component {
         const data = this.props.portfolioData;
 
         return (
-          <div>
-            <header
-              className={`site-header ${isSticky ? 'site-header--sticky' : ''}`}
-            >
-              <div className='site-header-wrapper'>
-                <div className='site-header-brand'>
-                  <Link
-                    className='site-header-link'
-                    activeClass='site-nav-link-active'
-                    to='home'
-                    href='#home'
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration={500}
-                  ></Link>
-                </div>
-                <div className='site-header-spacer'></div>
-                <button
-                  type='button'
-                  className='site-header-btn-mobile'
-                  onClick={this.openNavbar}
+            <div>
+                <header
+                    className={`site-header ${isSticky ? 'site-header--sticky' : ''}`}
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='18.338'
-                    height='12'
-                    viewBox='0 0 18.338 12'
-                    className={`site-header-burger-icon ${
-                      isSticky ? 'site-header-burger-icon-dark' : ''
+                    <div className='site-header__wrapper'>
+                        <div className='site-header__brand'>
+                            <Link
+                                className='site-header__link'
+                                activeClass='site-nav__link--active'
+                                to='home'
+                                href='#home'
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                            <img
+                                src = { isSticky ? data.logo : data.logoWhite }
+                                alt = ''
+                                className = 'site-header__logo'
+                            />
+                            </Link>
+                        </div>
+                        <div className='site-header__spacer'></div>
+                        <button
+                        type='button'
+                        className='site-header-btn-mobile'
+                        onClick={this.openNavbar}
+                        >
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='18.338'
+                            height='12'
+                            viewBox='0 0 18.338 12'
+                            className={`site-header-burger-icon ${
+                            isSticky ? 'site-header-burger-icon-dark' : ''
+                            }`}
+                        >
+                        <g
+                        id='group6148'
+                        data-name='group 6148'
+                        transform='translate(-288 -20)'
+                        >
+                            <rect
+                                id='rectangle2400'
+                                data-name='rectangle 2400'
+                                width='18.338'
+                                height='2'
+                                rx='1'
+                                transform='translate(288 20)'
+                                fill='#fff'
+                            ></rect>
+                            <rect
+                                id='rectangle2401'
+                                data-name='rectangle 2401'
+                                width='14.94'
+                                height='2'
+                                rx='1'
+                                transform='translate(288 25)'
+                                fill='#fff'
+                            ></rect>
+                            <rect
+                                id='rectangle2402'
+                                data-name='rectangle 2402'
+                                width='18.338'
+                                height='2'
+                                rx='1'
+                                transform='translate(288 30)'
+                                fill='#fff'
+                            ></rect>
+                        </g>
+                        </svg>
+                        </button>
+                        {isOpenSidebar && (
+                        <Fragment>
+                            {' '}
+                            <div className='site-nav-backdrop'></div>
+                        </Fragment>
+                        )}
+                        <div
+                        className={`site-navbar ${isOpenSidebar ? 'is-open' : ''}`}
+                        ></div>
+                    <ul
+                    className={`site-nav site-sidebar ${
+                        isOpenSidebar ? 'is-open' : ''
                     }`}
-                  >
-                    <g
-                      id='group6148'
-                      data-name='group 6148'
-                      transform='translate(-288 -20)'
                     >
-                      <rect
-                        id='rectangle2400'
-                        data-name='rectangle 2400'
-                        width='18.338'
-                        height='2'
-                        rx='1'
-                        transform='translate(288 20)'
-                        fill='#fff'
-                      ></rect>
-                      <rect
-                        id='rectangle2401'
-                        data-name='rectangle 2401'
-                        width='14.94'
-                        height='2'
-                        rx='1'
-                        transform='translate(288 25)'
-                        fill='#fff'
-                      ></rect>
-                      <rect
-                        id='rectangle2402'
-                        data-name='rectangle 2402'
-                        width='18.338'
-                        height='2'
-                        rx='1'
-                        transform='translate(288 30)'
-                        fill='#fff'
-                      ></rect>
-                    </g>
-                  </svg>
-                </button>
-                {isOpenSidebar && (
-                  <Fragment>
-                    {' '}
-                    <div className='site-nav-backdrop'></div>
-                  </Fragment>
-                )}
-                <div
-                  className={`site-navbar ${isOpenSidebar ? 'is-open' : ''}`}
-                ></div>
-                <ul
-                  className={`site-nav site-sidebar ${
-                    isOpenSidebar ? 'is-open' : ''
-                  }`}
-                >
-                    <li className='site-nav__list'>
-                        <Link
-                            onClick={this.onHandleClickLink}
-                            className='site-nav__link--active'
-                            to='project'
-                            href='#project'
-                            spy={true}
-                            smooth={true}
-                            offset={-20}
-                            duration={500}
-                        >
-                            Projects
-                        </Link>
-                    </li>
-                    <li className='site-nav__list'>
-                        <Link
-                            onClick={this.onHandleClickLink}
-                            className='site-nav__link--active'
-                            to='skills'
-                            href='#skills'
-                            spy={true}
-                            smooth={true}
-                            offset={-30}
-                            duration={500}
-                        >
-                            Skills
-                        </Link>
-                    </li>
-                    <li className='site-nav__list'>
-                        <Link
-                            onClick={this.onHandleClickLink}
-                            className='site-nav__link--active'
-                            to='experience'
-                            href='#experience'
-                            spy={true}
-                            smooth={true}
-                            offset={-30}
-                            duration={500}
-                        >
-                            Experience
-                        </Link>
-                    </li>
-                    <li className = 'site-nav__list'>
-                        <Link
-                        >
-                            Email: brian.ulatowski@gmail.com
-                        </Link>                        
-                        
-                  </li>
+                        <li className='site-nav__list'>
+                            <Link
+                                onClick={this.onHandleClickLink}
+                                className='site-nav__link--active'
+                                to='project'
+                                href='#project'
+                                spy={true}
+                                smooth={true}
+                                offset={-20}
+                                duration={500}
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li className='site-nav__list'>
+                            <Link
+                                onClick={this.onHandleClickLink}
+                                className='site-nav__link--active'
+                                to='skills'
+                                href='#skills'
+                                spy={true}
+                                smooth={true}
+                                offset={-30}
+                                duration={500}
+                            >
+                                Skills
+                            </Link>
+                        </li>
+                        <li className='site-nav__list'>
+                            <Link
+                                onClick={this.onHandleClickLink}
+                                className='site-nav__link--active'
+                                to='experience'
+                                href='#experience'
+                                spy={true}
+                                smooth={true}
+                                offset={-30}
+                                duration={500}
+                            >
+                                Experience
+                            </Link>
+                        </li>
+                        <li className = 'site-nav__list'>
+                            <Link
+                            >
+                                Email: brian.ulatowski@gmail.com
+                            </Link>                                                    
+                        </li>
                 </ul>
               </div>
             </header>
